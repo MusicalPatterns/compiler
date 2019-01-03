b89f38b2db216bf99d7c5e1690390c07bae6c89b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-01-03 15:54:43 -0800

    compiler snapshots now

---
+++ bin/snapshot.js
@@ -0,0 +1,18 @@
+const ESCAPE_CLI_PATTERN_NODE_MODULES_FOR_PATTERN_FOR_REQIRING = '../../../../'
+
+require(`${ESCAPE_CLI_PATTERN_NODE_MODULES_FOR_PATTERN_FOR_REQIRING}test/mockDom`)
+const { appendFileSync, existsSync, unlinkSync } = require('fs')
+const { compilePattern } = require('@musical-patterns/compiler')
+
+const updateSnapshot = async () => {
+    const { pattern } = require(`${ESCAPE_CLI_PATTERN_NODE_MODULES_FOR_PATTERN_FOR_REQIRING}src`)
+    const snapshot = await compilePattern(pattern)
+
+    const snapshotFile = 'src/snapshot.json'
+    existsSync(snapshotFile) && unlinkSync(snapshotFile)
+    appendFileSync(snapshotFile, JSON.stringify(snapshot, null, 2))
+}
+
+updateSnapshot()
+    .then()
+    .catch()

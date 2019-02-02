a2b8323c44d8724a36158d2020ee95b1f30f7a8e
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-02 12:14:47 -0800

    1.0.83: scales arent necessary

--- src/support/calculateNoteSpecsTotalCompiledDuration.ts
+++ src/support/calculateNoteSpecsTotalCompiledDuration.ts
@@ -2,8 +2,8 @@ import { from, Time, to } from '@musical-patterns/utilities'
 import { compileNoteProperty } from '../noteProperty'
 import { NotePropertySpec, NoteSpec, Scale } from '../types'
 
-const calculateNoteSpecsTotalCompiledDuration: (noteSpecs: NoteSpec[], scales: Scale[]) => Time =
-    (noteSpecs: NoteSpec[], scales: Scale[]): Time =>
+const calculateNoteSpecsTotalCompiledDuration: (noteSpecs: NoteSpec[], scales?: Scale[]) => Time =
+    (noteSpecs: NoteSpec[], scales?: Scale[]): Time =>
         noteSpecs.reduce(
             (totalDuration: Time, noteSpec: NoteSpec): Time => {
                 const durationSpec: NotePropertySpec = noteSpec.durationSpec || {}

745d73114d76bf7a789e80105b6696a7f822225b
Douglas Blumeyer <douglas.blumeyer@gmail.com>
2019-02-06 16:33:58 -0800

    1.0.90: negative

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 // tslint:disable no-magic-numbers
 
-import { to, Translation } from '@musical-patterns/utilities'
+import { negative, to, Translation } from '@musical-patterns/utilities'
 
-const DEFAULT_TRANSLATION_FOR_ALMOST_FULL_SUSTAIN: Translation = to.Translation(-0.1)
+const DEFAULT_TRANSLATION_FOR_ALMOST_FULL_SUSTAIN: Translation = to.Translation(negative(0.1))
 
 export {
     DEFAULT_TRANSLATION_FOR_ALMOST_FULL_SUSTAIN,
